fa52cdf025a08daf811ebb451de1b8635b24c851
marco <119810706+array-marco@users.noreply.github.com>
2024-05-02 12:28:34 +0300

    widget builder

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_test/views/layouts/adaptive_layout.dart';
 import 'package:responsive_test/views/layouts/desktop_layout.dart';
 import 'package:responsive_test/views/layouts/tablet_layout.dart';
 import 'package:responsive_test/views/layouts/phone_layout.dart';
@@ -8,15 +9,9 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constrains) {
-      
-      if (constrains.maxWidth < 600) {
-        return const PhoneLayout();
-      } else if (constrains.maxWidth < 900) {
-        return const TabletLayout();
-      } else {
-        return const DesktopLayout();
-      }
-    });
+    return AdaptiveLayout(
+        mobileLayout: (context) => const PhoneLayout(),
+        tabletLayout: (context) => const TabletLayout(),
+        desktopLayout: (context) => const DesktopLayout());
   }
 }
